fd3e8717159faa7635bb05bcb5b887a8989e6377
RaW <akram.iiuc.ctg@gmail.com>
2024-03-07 20:42:53 +0600

    Finish

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -130,5 +130,15 @@ router.get("/user", async (req, res) => {
     }
 });
 
+router.get("/user/:email", async (req, res) => {
+    try {
+        const email = req.params.email
+        const user = await userCollection.findOne({ email })
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." });
+    }
+});
+
 
 module.exports = router;
